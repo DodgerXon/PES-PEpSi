46ecc6ae03081bc8e9bc476b48227b21744136d3
Blinksy22 <133363356+Blinksy22@users.noreply.github.com>
2024-05-01 14:09:05 +0200

    Updated code (RGB_lamp)
    update

---
+++ Wemos/RGB_Lamp/ledRGB.cpp
@@ -0,0 +1,11 @@
+// #include "ledRGB.h"
+
+// LedRGB::LedRGB(int r, int g, int b ) : red(r), green(g), blue(b) {
+// }
+// //WemosServer::~WemosServer() {}
+
+// void LedRGB::zetLedAan(int r,int g,int b) {
+//   analogWrite(ledR, r * 4);
+//   analogWrite(ledG, g * 4);
+//   analogWrite(ledB, b * 4);
+// }
