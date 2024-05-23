dba33f066f8ccd916e60ee9a45ce84032eca5b22
ArjanKui <132342766+ArjanKui@users.noreply.github.com>
2024-05-23 12:15:24 +0200

    Add files via upload
    Servo nieuwe versie zonder bewegingssensor

---
+++ Wemos/ServoNieuweVersie/Server.h
@@ -0,0 +1,17 @@
+#ifndef SERVER_H
+#define SERVER_H 
+
+#include <ESP8266WiFi.h>
+
+using namespace std;
+
+class WemosServer {
+public:
+    WemosServer();
+    void verbindenWifi();
+    bool startServer();
+    String receivedMsg();
+    void sendMsg(String);
+};
+
+#endif
---
+++ Wemos/ServoNieuweVersie/ServoNieuweVersie.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+#include "Server.h"
+
+#define SERVO_PIN D7
+
+Servo servo;
+WemosServer wServer;
+bool isOpen;
+String command;
+
+void setup() {
+  Serial.begin(9600);
+  servo.attach(SERVO_PIN);
+  servo.write(0);
+  wServer.verbindenWifi();
+}
+
+void loop() {
+  String command;
+  String deStatus = "";
+  if(wServer.startServer()){
+    command = wServer.receivedMsg();
+    Serial.println(command);
+    if(command == "status"){
+      wServer.sendMsg(deStatus);
+    }
+    else if(command == "open"){ 
+        servo.write(180);
+        deStatus = "open";
+    }
+    else if(command == "sluit"){
+        servo.write(0);
+        deStatus = "sluit";
+    }
+  }
+}
