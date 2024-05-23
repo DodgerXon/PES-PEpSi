d5dee71e7dea81940e44b0653a0f8a3ba0c49ba5
Blinksy22 <133363356+Blinksy22@users.noreply.github.com>
2024-05-23 15:28:34 +0200

    Nieuw

---
+++ Wemos/BeweegSensorDeur/Server.h
@@ -0,0 +1,22 @@
+#ifndef SERVER_H
+#define SERVER_H 
+
+#include <ESP8266WiFi.h>
+#include <time.h>
+
+using namespace std;
+
+class WemosServer {
+public:
+    WemosServer();
+    ~WemosServer();
+    void verbindenWifi();
+    void startServer();
+    void sendMsg(String);
+    String receivedMsg();
+
+private:
+
+};
+
+#endif
---
+++ Wemos/BeweegSensorRGB/BeweegSensorRGB.ino
@@ -0,0 +1,60 @@
+#include "Server.h"
+
+using namespace std;
+
+WemosServer wServer;
+
+const int sensorPin = 5;  // D0
+const int boardLed = 2;     // D4
+
+int sensorState = 0;
+
+int teller = 0;
+
+void setup() {
+  Serial.begin(9600);
+  delay(10);
+
+  pinMode(boardLed, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
+
+  pinMode(sensorPin, INPUT_PULLUP);
+  digitalWrite(boardLed, LOW);
+
+  wServer.verbindenWifi();
+
+}
+
+void loop() {
+
+  teller++;
+  char buf[20];
+
+  wServer.startServer();
+  String received = wServer.receivedMsg(); // Deze functie veroorzaakt een delay
+  
+  received.toCharArray(buf, received.length()+1);
+
+  //Serial.print("main: "); Serial.println(buf);
+
+  //sscanf(buf, "%d %d %d", &red, &green, &blue);
+
+  sensorState = digitalRead(sensorPin);
+
+  if (received == "status") {
+    if (sensorState) {
+      wServer.sendMsg("Aan");
+    } else {
+      wServer.sendMsg("Uit");
+    }
+  }
+
+  if (sensorState) {
+    digitalWrite(boardLed, HIGH);
+  } else {
+    digitalWrite(boardLed, LOW);
+  }
+
+  Serial.print("Sensor: "); Serial.println(sensorState);
+
+  delay(10);
+}
