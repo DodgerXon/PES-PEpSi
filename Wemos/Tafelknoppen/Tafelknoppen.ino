f510bdaf9238d6f6adb84bca92e0647ebea290a8
Blinksy22 <133363356+Blinksy22@users.noreply.github.com>
2024-05-29 15:35:00 +0200

    fix

--- Wemos/Tafelknoppen/Tafelknoppen.ino
+++ Wemos/Tafelknoppen/Tafelknoppen.ino
@@ -4,6 +4,8 @@ const int t1 = D1;
 const int t2 = D2;
 const int t3 = D3;
 
+const int boardLed = 2; // D4
+
 int buttonStatusT1 = 0;
 int buttonStatusT2 = 0;
 int buttonStatusT3 = 0;
@@ -14,6 +16,10 @@ void setup() {
     pinMode(t1, INPUT_PULLUP);
     pinMode(t2, INPUT_PULLUP);
     pinMode(t3, INPUT_PULLUP);
+
+    pinMode(boardLed, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
+    digitalWrite(boardLed, LOW);
+
     Serial.begin(9600);
     wemos.verbindenWifi();
 }
@@ -26,24 +32,24 @@ void loop() {
     delay(10);
     if (buttonStatusT1 != pinValueT1) {
         buttonStatusT1 = pinValueT1;
-        Serial.println("T1 " + buttonStatusT1);
         if (buttonStatusT1 == HIGH) {
+            Serial.print("T1 ");
             if (wemos.startServer()) {
                 wemos.sendMsg("t1");
             }
         }
     } else if (buttonStatusT2 != pinValueT2) {
         buttonStatusT2 = pinValueT2;
-        Serial.println("T2 " + buttonStatusT2);
         if (buttonStatusT2 == HIGH) {
+            Serial.print("T2 ");
             if (wemos.startServer()) {
                 wemos.sendMsg("t2");
             }
         }
     } else if (buttonStatusT3 != pinValueT3) {
         buttonStatusT3 = pinValueT3;
-        Serial.println("T3 " + buttonStatusT3);
         if (buttonStatusT3 == HIGH) {
+            Serial.print("T3 ");
             if (wemos.startServer()) {
                 wemos.sendMsg("t3");
             }
